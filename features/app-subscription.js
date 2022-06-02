205c97ed87a9c5ca44a3867770aa7a9afd470691
ruanrossato-viseu <ruan.rossato@grupoviseu.com.br>
2022-06-02 12:23:14 -0300

    Decluttering and breaking down big messages

--- features/app-subscription.js
+++ features/app-subscription.js
@@ -17,13 +17,17 @@ module.exports = function(controller) {
         }
     });
 
-    
-    // Usuários com o aplicativo instalado, *Fazer o bot esperar 5 minutos e enviar esta mensagem
-    flow.addQuestion({
-        "type":"buttons",
-        "section":"Subscription",
-        "body":"Ótimo! Agora, vou precisar que você autorize o saque-aniversário dentro do aplicativo FGTS. Faça o cadastro no aplicativo, e depois é só optar pelo saque-aniversário.\
-        \n\n1 - Abra o aplicativo do FGTS\
+    flow.addMessage({
+        "type":"message",
+        "section":"subscription",
+        "body":"Ótimo! Agora, vou precisar que você autorize o saque-aniversário dentro do aplicativo FGTS. Faça o cadastro no aplicativo, e depois é só optar pelo saque-aniversário"           
+        },
+        "subscription")
+
+    flow.addMessage({
+        "type":"message",
+        "section":"subscription",
+        "body":"1 - Abra o aplicativo do FGTS\
         \n2 - Aperta no botão Entrar no aplicativo\
         \n3 - Digite seu CPF e marque a caixinha Não sou um robô\
         \n4 - Informe sua senha e aperte em entrar\
@@ -31,11 +35,16 @@ module.exports = function(controller) {
         \n6 - Leia as cláusulas e aceite\
         \n5 - Selecione a opção Saque Aniversário do FGTS\
         \n6 - Leia com atenção os termos de adesão e aceite\
-        \n7 - Escolha a Modalidade saque aniversário\
-        \n\nQuando acabar, me chama para continuarmos.",
+        \n7 - Escolha a Modalidade saque aniversário"           
+        },
+        "subscription")
+    
+    flow.addQuestion({
+        "type":"buttons",
+        "section":"subscription",
+        "body":"Quando acabar, me chama para continuarmos.",
         "footer":"Clique na opção desejada",
-        // "header":"",
-
+        
         "buttons":[
             {
                 "text": "Acabei",
@@ -45,26 +54,17 @@ module.exports = function(controller) {
                 "text": "Preciso de ajuda",
                 "payload": "nao"
             }
-        ],
-        
-        // "media":
-        //     {
-        //         "contentType": "image|video|document",
-        //         "mediaURL":"",
-        //         "mediaID":"",
-        //         "caption":"",
-        //         "filename":""
-        //     }
+        ]
         },
-    async(response,flow,bot) => {
-        
-    },
-    "inscricaoSaqueAniversario",
-    "subscription")
+        async(response,flow,bot) => {
+            
+        },
+        "inscricaoSaqueAniversario",
+        "subscription")
 
     flow.addQuestion({
         "type":"buttons",
-        "section":"Subscription",
+        "section":"subscription",
         "body":"Deu certo?",
         "footer":"Clique na opção desejada",
         // "header":"",
@@ -78,24 +78,15 @@ module.exports = function(controller) {
                 "text": "Não",
                 "payload": "nao"
             }
-        ],
-        
-        // "media":
-        //     {
-        //         "contentType": "image|video|document",
-        //         "mediaURL":"",
-        //         "mediaID":"",
-        //         "caption":"",
-        //         "filename":""
-        //     }
+        ]
         },
-    async(response,flow,bot) => {
-        if(nlu.checkAffirmative(response)) {
-          await flow.gotoThread("subscriptionFgts")
-        }
-        else {
-            await flow.gotoThread("subscriptionAgain")
-        }
+        async(response,flow,bot) => {
+            if(nlu.checkAffirmative(response)) {
+            await flow.gotoThread("subscriptionFgts")
+            }
+            else {
+                await flow.gotoThread("subscriptionAgain")
+            }
     },
     "inscricaoSaqueAniversario",
     "subscription")
@@ -103,7 +94,7 @@ module.exports = function(controller) {
     flow.addQuestion(
         {
             "type":"buttons",
-            "section":"Subscription",
+            "section":"subscription",
             "body":"Se preferir temos um passo a passo em vídeo, *basta clicar no link:* https://youtu.be/tuXPnjnu33Q \
             \n\nDepois que finalizar, me avisa se deu certo, por favor.",
             "footer":"Clique na opção desejada",
@@ -118,16 +109,9 @@ module.exports = function(controller) {
                     "text": "Preciso de ajuda",
                     "payload": "nao"
                 }
-            ],
+            ]
             
-            // "media":
-            //     {
-            //         "contentType": "image|video|document",
-            //         "mediaURL":"",
-            //         "mediaID":"",
-            //         "caption":"",
-            //         "filename":""
-            //     }
+          
             },
     async(response,flow,bot) => {
         if(nlu.checkAffirmative(response)) {
@@ -140,22 +124,32 @@ module.exports = function(controller) {
     "inscricaoSaqueAniversario",
     "subscriptionAgain")
 
+    flow.addMessage({
+        "type":"message",
+        "section":"subscription",
+        "body":"Certo, estamos quase no fim! Agora, por favor *autorize o _Banco Pan_ a consultar o seu FGTS* dentro do app do FGTS.\
+        \nSe tiver alguma dúvida, também tenho um passo-a-passo explicando:"           
+        },
+        "subscriptionFgts")
 
+     flow.addMessage({
+        "type":"message",
+        "section":"subscription",
+        "body":"1 - Abra o aplicativo do FGTS\
+        \n2 - Aperta no botão *Entrar no aplicativo*\
+        \n3 - Digite seu CPF e marque a caixinha *Não sou um robô*\
+        \n4 - Informe sua senha e aperte em *entrar*\
+        \n5 - Na tela inicial, seleciona a opção *autorizar bancos a consultarem seu FGTS*\
+        \n6 - Selecione a opção *empréstimo saque aniversário*\
+        \n7 - Selecione o *Banco Pan*\
+        \n8 - Confirme no botão *Confirmar seleção*"           
+        },
+        "subscriptionFgts")
     flow.addQuestion(
         {
             "type":"buttons",
-            "section":"Subscription",
-            "body":"Certo, estamos quase no fim! Agora, por favor *autorize o _Banco Pan_ a consultar o seu FGTS* dentro do app do FGTS.\
-            \nSe tiver alguma dúvida, também tenho um passo-a-passo explicando:\
-            \n\n1 - Abra o aplicativo do FGTS\
-            \n2 - Aperta no botão *Entrar no aplicativo*\
-            \n3 - Digite seu CPF e marque a caixinha *Não sou um robô*\
-            \n4 - Informe sua senha e aperte em *entrar*\
-            \n5 - Na tela inicial, seleciona a opção *autorizar bancos a consultarem seu FGTS*\
-            \n6 - Selecione a opção *empréstimo saque aniversário*\
-            \n7 - Selecione o *Banco Pan*\
-            \n8 - Confirme no botão *Confirmar seleção*\
-            \n\nDeu certo?",
+            "section":"subscription",
+            "body":"Deu certo?",
             "footer":"Clique na opção desejada",
             // "header":"",
     
@@ -168,16 +162,7 @@ module.exports = function(controller) {
                     "text": "Não",
                     "payload": "nao"
                 }
-            ],
-            
-            // "media":
-            //     {
-            //         "contentType": "image|video|document",
-            //         "mediaURL":"",
-            //         "mediaID":"",
-            //         "caption":"",
-            //         "filename":""
-            //     }
+            ]
             },
     async(response,flow,bot) => {
         if(nlu.checkAffirmative(response)) {
@@ -192,7 +177,7 @@ module.exports = function(controller) {
     flow.addQuestion(
         {
             "type":"buttons",
-            "section":"Subscription",
+            "section":"subscription",
             "body":"Então dê uma olhada no *passo-a-passo em vídeo* nesse link aqui: _https://youtu.be/B1GFwRNnI9U_ \
             \n\nDepois que finalizar, *me avisa aqui*, por favor",
             "footer":"Deu certo?",
@@ -228,39 +213,6 @@ module.exports = function(controller) {
     "inscricaoConsultarFgts",
     "subscriptionFgtsAgain")
 
-    // flow.addQuestion(
-    //     {
-    //         "type":"message",
-    //         "section":"Subscription",
-    //         "body":"Conseguiu?",
-    //         "footer":"Clique na opção desejada",
-            
-    
-    //         "buttons":[
-    //             {
-    //                 "text": "Sim",
-    //                 "payload": "sim"
-    //             },
-    //             {
-    //                 "text": "Não",
-    //                 "payload": "nao"
-    //             }
-    //         ],
-            
-    //         "media":
-    //             {
-    //                 "contentType": "image|video|document",
-    //                 "mediaURL":"",
-    //                 "mediaID":"",
-    //                 "caption":"",
-    //                 "filename":""
-    //             }
-    //         },
-    // async(response,flow,bot) => {
-       
-    // },
-    // "inscricaoConsultarFgts",
-    // "subscriptionFgtsAgain")
 
     flow.after(async (vars, bot) => {
         await bot.cancelAllDialogs();
