f8fb9d62f36e511c28197b8bca3f69f3e560b8e1
ruanrossato-viseu <ruan.rossato@grupoviseu.com.br>
2022-06-02 02:59:29 -0300

    Creates Banks Script
    Script for integration with simulation and sign-up API's

---
+++ scripts/banks.js
@@ -0,0 +1,44 @@
+
+const process = require("process");
+const { parse } = require("dotenv");
+const axios = require("axios");
+require("dotenv").config();
+
+
+
+module.exports.simulation = function simulation(cpf){
+    cpf = cpf.replace(/\D/g,'');
+    cpf = cpf.substr(0, 3) + "." + str.substr(3,6)+"."+str.substr(6,9)+"-"+str.substr(9);
+    var data = JSON.stringify({
+        "cpf": cpf,
+        "source": "chatbot",
+        "value": 0,
+        "bankPan": true
+    });
+
+    var config = {
+        method: 'post',
+        url: 'https://tr5bceh9i1.execute-api.sa-east-1.amazonaws.com/DEV/simulation',
+        headers: { 
+            'x-api-key': '8HOhYvOk8z8AoMoPU6o0krG8IUKeVf38IvoHnTkb', 
+            'Content-Type': 'application/json'
+        },
+        data : data
+    };
+
+
+    var simulationInfo = await axios(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data))
+            return(response.data)
+        })
+        .catch( function(error){
+            
+            console.log(error);
+            return false
+        });
+    
+    
+
+    return simulationInfo
+}
